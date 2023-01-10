48e80a388e17f632310ee6e36936c34470ab33b4
Hady Willi <hady.willi@gojek.com>
2023-01-10 18:13:00 +0000

    fix name

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-podman-remote build --tag docker.io/hjwalt/cx-base-test-2:latest --format docker cx-base/.
+podman-remote build --tag docker.io/hjwalt/cx-base:latest --format docker cx-base/.
 podman-remote build --tag docker.io/hjwalt/cx-python-aws:latest --format docker cx-python-aws/.
